913fb492d97f919e820984bd2a2498657048d2c0
Martí <marti.corrales.andreu@gmail.com>
2025-11-06 19:22:22 +0100

    Prueba

---
+++ src/AEA3/test.java
@@ -0,0 +1,13 @@
+package AEA3;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("¿Como te llamas?");
+        System.out.println("¡Encantado de conocerte " + s.nextLine());
+
+    }
+}
